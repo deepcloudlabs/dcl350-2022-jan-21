0db18e1f2792a58d4ca43098b3a46cdd99ac2185
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-21 15:41:25 +0300

    initial commit

--- hr-domain/src/com/example/hr/domain/TcKimlikNo.java
+++ hr-domain/src/com/example/hr/domain/TcKimlikNo.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 //  i) has no identity
 // ii) immutable
 public final class TcKimlikNo {
-	private final static Map<String,TcKimlikNo> cache=new HashMap<>();
+	private final static Map<String, TcKimlikNo> cache = new HashMap<>();
 	private final String value;
 
 	private TcKimlikNo(String value) {
@@ -16,9 +16,8 @@ public final class TcKimlikNo {
 	}
 
 	public static TcKimlikNo valueOf(String value) {
-		if (!isValid(value)) 
-			throw new IllegalArgumentException(
-					"This is not a valid identity no!");
+		if (!isValid(value))
+			throw new IllegalArgumentException("This is not a valid identity no!");
 		var tcKimlikNo = cache.get(value);
 		if (Objects.isNull(tcKimlikNo)) {
 			tcKimlikNo = new TcKimlikNo(value);
@@ -28,6 +27,34 @@ public final class TcKimlikNo {
 	}
 
 	private static boolean isValid(String value) {
+		if (value == null)
+			return false;
+		if (!value.matches("^\\d{11}$")) { // fail-fast
+			return false;
+		}
+		int[] digits = new int[11];
+		for (int i = 0; i < digits.length; ++i) {
+			digits[i] = value.charAt(i) - '0';
+		}
+		int x = digits[0];
+		int y = digits[1];
+		for (int i = 1; i < 5; i++) {
+			x += digits[2 * i];
+		}
+		for (int i = 2; i <= 4; i++) {
+			y += digits[2 * i - 1];
+		}
+		int c1 = 7 * x - y;
+		if (c1 % 10 != digits[9]) {
+			return false;
+		}
+		int c2 = 0;
+		for (int i = 0; i < 10; ++i) {
+			c2 += digits[i];
+		}
+		if (c2 % 10 != digits[10]) {
+			return false;
+		}
 		return true;
 	}
 
@@ -39,21 +66,5 @@ public final class TcKimlikNo {
 	public String toString() {
 		return "TcKimlikNo [value=" + value + "]";
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
