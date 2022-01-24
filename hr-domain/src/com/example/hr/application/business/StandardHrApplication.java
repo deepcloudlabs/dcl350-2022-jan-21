ea6c866a359fc1a1b6825ac195d4d9c6137acbf8
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-24 12:23:11 +0300

    add events and event publisher

--- hr-domain/src/com/example/hr/application/business/StandardHrApplication.java
+++ hr-domain/src/com/example/hr/application/business/StandardHrApplication.java
@@ -3,33 +3,38 @@ package com.example.hr.application.business;
 import java.util.Optional;
 
 import com.example.hr.application.HrApplication;
+import com.example.hr.application.business.events.EmployeeHiredEvent;
 import com.example.hr.application.business.exception.EmployeeNotFoundException;
 import com.example.hr.application.business.exception.ExistingEmployeeException;
 import com.example.hr.domain.Employee;
 import com.example.hr.domain.TcKimlikNo;
+import com.example.hr.infrastructure.EventPublisher;
 import com.example.hr.repository.EmployeeRepository;
 
 public class StandardHrApplication implements HrApplication {
 	private EmployeeRepository employeeRepository;
-	
-	public StandardHrApplication(EmployeeRepository employeeRepository) {
+	private EventPublisher eventPublisher;
+
+	public StandardHrApplication(EmployeeRepository employeeRepository, EventPublisher eventPublisher) {
 		this.employeeRepository = employeeRepository;
+		this.eventPublisher = eventPublisher;
 	}
 
 	@Override
 	public Employee hireEmployee(Employee employee) {
 		var kimlik = employee.getTcKimlikNo();
 		if (employeeRepository.existsByKimlikNo(kimlik))
-			throw new ExistingEmployeeException(
-					"Employee already exist", kimlik.getValue());
-		return employeeRepository.save(employee);
+			throw new ExistingEmployeeException("Employee already exist", kimlik.getValue());
+		Employee savedEmployee = employeeRepository.save(employee);
+		var businessEvent = new EmployeeHiredEvent(savedEmployee);
+		eventPublisher.publishEvent(businessEvent);
+		return savedEmployee;
 	}
 
 	@Override
 	public Optional<Employee> fireEmployee(TcKimlikNo kimlik) {
 		if (!employeeRepository.existsByKimlikNo(kimlik))
-			throw new EmployeeNotFoundException(
-					"Employee does not exist", kimlik.getValue());
+			throw new EmployeeNotFoundException("Employee does not exist", kimlik.getValue());
 		return employeeRepository.remove(kimlik);
 	}
 
