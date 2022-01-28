d125ce026528a7a033d36c6062a08c8753cccf9c
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-28 15:41:16 +0300

    initial commit

--- crm-reactive-microservice/src/main/java/com/example/crm/controller/CrmReactiveController.java
+++ crm-reactive-microservice/src/main/java/com/example/crm/controller/CrmReactiveController.java
@@ -1,14 +1,20 @@
 package com.example.crm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.crm.dto.request.AcquireCustomerRequest;
 import com.example.crm.dto.response.CustomerResponse;
 import com.example.crm.service.ReactiveCustomerService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,5 +26,24 @@ public class CrmReactiveController {
 	@GetMapping("{identity}")
 	public Mono<CustomerResponse> findCustomerByIdentity(@PathVariable String identity) {
 		return reactiveCustomerService.findById(identity);
+	}
+	
+	@GetMapping
+	public Flux<CustomerResponse> findCustomersByPage(
+			@RequestParam int page,
+			@RequestParam int size
+			) {
+		return reactiveCustomerService.findAllByPage(page,size);
+	}
+	
+	@PostMapping
+	public Mono<CustomerResponse> acquireCustomer(
+			@RequestBody AcquireCustomerRequest request){
+		return reactiveCustomerService.acquireCustomer(request);
+	}
+	
+	@DeleteMapping("{identity}")
+	public Mono<CustomerResponse> releaseCustomerByIdentity(@PathVariable String identity) {
+		return reactiveCustomerService.releaseCustomer(identity);
 	}
 }
--- crm-reactive-microservice/src/main/java/com/example/crm/service/ReactiveCustomerService.java
+++ crm-reactive-microservice/src/main/java/com/example/crm/service/ReactiveCustomerService.java
@@ -2,14 +2,29 @@ package com.example.crm.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.crm.dto.request.AcquireCustomerRequest;
 import com.example.crm.dto.response.CustomerResponse;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class ReactiveCustomerService {
 
 	public Mono<CustomerResponse> findById(String identity) {
+		return null;
+	}
+
+	public Flux<CustomerResponse> findAllByPage(int page, int size) {
+		return null;
+	}
+
+	public Mono<CustomerResponse> acquireCustomer(AcquireCustomerRequest request) {
+		return null;
+	}
+
+	public Mono<CustomerResponse> releaseCustomer(String identity) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
