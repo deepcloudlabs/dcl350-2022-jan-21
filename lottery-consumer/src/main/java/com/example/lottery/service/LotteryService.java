d448b47a9ef7333d98875581da977dbac38cc77c
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-28 10:09:38 +0300

    initial commit

--- lottery-consumer/src/main/java/com/example/lottery/service/LotteryService.java
+++ lottery-consumer/src/main/java/com/example/lottery/service/LotteryService.java
@@ -4,10 +4,23 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @FeignClient(name = "lottery")
+@RequestMapping("/lottery/api/v1")
 public interface LotteryService {
-	@GetMapping("/lottery/api/v1/numbers")
+	@GetMapping("/numbers")
+	@Retry(name="lottery",fallbackMethod = "getNumbersFallback")
 	List<List<Integer>> getNumbers(@RequestParam int column);
+	
+	default public List<List<Integer>> getNumbersFallback(
+			int column,Throwable e) {
+		return List.of(
+			    List.of(1,2,3,4,5,6),
+			    List.of(4,8,15,16,23,42)		    
+			);
+		}
 }
