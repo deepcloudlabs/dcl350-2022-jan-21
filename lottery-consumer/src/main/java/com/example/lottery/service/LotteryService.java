79a6b4fc364694b5bde9380f1b8b3c47c196a13a
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-28 12:46:53 +0300

    initial commit

--- lottery-consumer/src/main/java/com/example/lottery/service/LotteryService.java
+++ lottery-consumer/src/main/java/com/example/lottery/service/LotteryService.java
@@ -7,12 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @FeignClient(name = "lottery")
 public interface LotteryService {
 	@GetMapping("/lottery/api/v1/numbers")
 	@Retry(name="lottery",fallbackMethod = "getNumbersFallback")
+	@RateLimiter(name="lottery",fallbackMethod = "getNumbersFallback")
+//	@TimeLimiter(name="lottery",fallbackMethod = "getNumbersFallback")
+	@CircuitBreaker(name="lottery",fallbackMethod = "getNumbersFallback")
+	@Bulkhead(name="lottery",fallbackMethod = "getNumbersFallback")
 	List<List<Integer>> getNumbers(@RequestParam int column);
 	
 	default public List<List<Integer>> getNumbersFallback(
